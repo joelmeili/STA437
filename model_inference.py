8928a63e95f4d8a95d45a4936584630f82a00ecd
Joël Meili <joel.meili@gmail.com>
2022-06-08 12:02:11 +0200

    udpates

--- model_inference.py
+++ model_inference.py
@@ -39,8 +39,9 @@ img = [img[i] for i in range(len(img)) if i in max_values]
 ground_truth = [ground_truth[i] for i in range(len(ground_truth)) if i in max_values]
 pred_mask = [pred_mask[i] for i in range(len(pred_mask)) if i in max_values]
 
+for i in range(1):
+    image = img[i]
+    truth = ground_truth[i]
+    mask = pred_mask[i]
 
-fig1 = plt.imshow(ground_truth[0])
-fig2 = plt.imshow(pred_mask[0])
-fig1.savefig("test.png")
-fig2.savefig("test2.png")
+    print(np.transpose(image))
