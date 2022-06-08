a99d3bd956a953d44cd4204c9a263711f59f5c16
Joël Meili <joel.meili@gmail.com>
2022-06-08 20:44:41 +0200

    updates

--- main.py
+++ main.py
@@ -25,7 +25,10 @@ from monai.transforms import (
     RandRotate90d,
     ScaleIntensityd,
     EnsureTyped,
-    EnsureType) 
+    EnsureType)
+
+
+best_score = 0.0
 
 def custom_collate(batch):
     images = torch.cat([torch.as_tensor(np.transpose(item_["img"], (3, 0, 1, 2))) for item in batch for item_ in item], 0).contiguous()
@@ -52,19 +55,75 @@ def create_data_splits(train_split, tets_split):
 
     return train_files, val_files, test_files
 
-if __name__ == "__main__":    
-    # HYPERPARAMETERS
-    train_splits = [0.5, 0.7]
-    test_split = 0.1
-    batch_sizes = [2, 4]
-    learning_rates = [1e-4, 1e-5]
-    optimizers = ["adam", "SGD"]
-    dropouts = [0.2, 0.5]
-    architectures = ["resnet34", "resnet50"]
-
-    best_model = ""
-    best_score = 0.0
+def train_model(train_loader, valid_loader, test_loader, name = "base_line", opt = "adam", 
+dropout = 0.2, architecture = "resnet34", encoder_weights = "imagenet", lr = 1e-4):
     
+    model = smp.FPN(encoder_name = architecture,
+                classes = 1,
+                encoder_weights = encoder_weights,
+                in_channels = 1,
+                activation = "sigmoid",
+                decoder_dropout = dropout)
+
+    writer = SummaryWriter(log_dir = "runs/" + name + ".pth")
+
+    loss = smp.utils.losses.DiceLoss()
+
+    metrics = [
+    smp.utils.metrics.IoU(threshold = 0.5)
+    ]
+
+    if opt == "adam":
+        optimizer = torch.optim.Adam([
+            dict(params = model.parameters(), lr = lr)])
+
+    elif opt == "SGD":
+        optimizer = torch.optim.SGD([
+            dict(params = model.parameters(), lr = lr, momentum = 0.9)])
+    
+    train_epoch = smp.utils.train.TrainEpoch(
+        model,
+        loss = loss,
+        metrics = metrics,
+        optimizer = optimizer,
+        device = "cuda",
+        verbose = True)
+
+    valid_epoch = smp.utils.train.ValidEpoch(
+        model,
+        loss = loss,
+        metrics = metrics,
+        device = "cuda",
+        verbose = True)
+
+    # train model for 40 epochs
+    max_score = 0
+    
+    for i in range(0, 20):
+
+        print("\nEpoch: {}".format(i))
+        train_logs = train_epoch.run(train_loader)
+        valid_logs = valid_epoch.run(val_loader)
+
+        writer.add_scalar("Loss/Train", train_logs["dice_loss"], i)
+        writer.add_scalar("Loss/Valid", valid_logs["dice_loss"], i)
+
+        writer.add_scalar("Score/Train", train_logs["iou_score"], i)
+        writer.add_scalar("Score/Valid", valid_logs["iou_score"], i)
+        
+        # do something (save model, change lr, etc.)
+        if max_score < valid_logs["iou_score"]:
+            max_score = valid_logs["iou_score"]
+            torch.save(model, "models/" + name + ".pth")
+
+        if max_score > best_score:
+            torch.save(model, "models/best_model.pth")
+            torch.save(test_loader, "test_loader_for_inference.pth")
+
+    writer.flush()         
+
+
+if __name__ == "__main__":       
     # define transforms for image and segmentation
     train_transforms = Compose([
         LoadImaged(keys = ["img", "seg"]),
@@ -72,9 +131,18 @@ if __name__ == "__main__":
         ScaleIntensityd(keys = ["img", "seg"]),
         RandCropByPosNegLabeld(
         keys=["img", "seg"], label_key = "seg", spatial_size=[512, 512, 1], pos = 2, neg = 1, num_samples = 8
+        )
+    ])
+
+    train_transforms_augmented = Compose([
+        LoadImaged(keys = ["img", "seg"]),
+        AddChanneld(keys = ["img", "seg"]),
+        ScaleIntensityd(keys = ["img", "seg"]),
+        RandCropByPosNegLabeld(
+        keys=["img", "seg"], label_key = "seg", spatial_size=[512, 512, 1], pos = 2, neg = 1, num_samples = 8
         ),
-        #RandRotate90d(keys=["img", "seg"], prob=0.5, spatial_axes=[0, 1]),
-        #EnsureTyped(keys=["img", "seg"]),
+        RandRotate90d(keys=["img", "seg"], prob=0.5, spatial_axes=[0, 1]),
+        EnsureTyped(keys=["img", "seg"]),
     ])
     
     val_transforms = Compose([
@@ -93,102 +161,36 @@ if __name__ == "__main__":
         keys=["img", "seg"], label_key = "seg", spatial_size = [512, 512, 1], pos = 3, neg = 1, num_samples = 8
     )])
 
-    for batch_size in batch_sizes:
+    # run baseline model
+    train_split = 0.7
+    test_split = 0.1
+    batch_size = 2
+    architecture = "resnet34"
+    encoder_weights = "imagenet"
+    optimizer = ""
+    dropout = 2
 
-        for train_split in train_splits:
+    train_files, val_files, test_files = create_data_splits(train_split, test_split)
+    
+    # create a training data loader
+    train_ds = monai.data.Dataset(data = train_files, transform = train_transforms)
 
-            train_files, val_files, test_files = create_data_splits(train_split, test_split)
-            
-            # create a training data loader
-            train_ds = monai.data.Dataset(data = train_files, transform = train_transforms)
+    # use batch_size=2 to load images and use RandCropByPosNegLabeld to generate 2 x 4 images for network training
+    train_loader = DataLoader(
+    train_ds,
+    batch_size = batch_size,
+    shuffle = True,
+    num_workers = 8,
+    collate_fn = custom_collate,
+    pin_memory = torch.cuda.is_available())
+    
+    # create a validation data loader
+    val_ds = monai.data.Dataset(data = val_files, transform = val_transforms)
+    val_loader = DataLoader(val_ds, batch_size = batch_size, num_workers = 8, shuffle = False, collate_fn = custom_collate)
 
-            # use batch_size=2 to load images and use RandCropByPosNegLabeld to generate 2 x 4 images for network training
-            train_loader = DataLoader(
-            train_ds,
-            batch_size = batch_size,
-            shuffle = True,
-            num_workers = 8,
-            collate_fn = custom_collate,
-            pin_memory = torch.cuda.is_available())
-            
-            # create a validation data loader
-            val_ds = monai.data.Dataset(data = val_files, transform = val_transforms)
-            val_loader = DataLoader(val_ds, batch_size = batch_size, num_workers = 8, shuffle = False, collate_fn = custom_collate)
+    # create a test data loader
+    test_ds = monai.data.Dataset(data = test_files, transform = test_transforms)
+    test_loader = DataLoader(test_ds, batch_size = batch_size, num_workers = 8, shuffle = False, collate_fn = custom_collate)     
 
-            # create a test data loader
-            test_ds = monai.data.Dataset(data = test_files, transform = test_transforms)
-            test_loader = DataLoader(test_ds, batch_size = batch_size, num_workers = 8, shuffle = False, collate_fn = custom_collate)
-            
-            for opt in optimizers:
-
-                for lr in learning_rates:
-                    
-                    for architecture in architectures:
-
-                        for dropout in dropouts:
-                    
-                            model = smp.FPN(encoder_name = architecture,
-                                        classes = 1,
-                                        encoder_weights = "imagenet",
-                                        in_channels = 1,
-                                        activation = "sigmoid",
-                                        decoder_dropout = dropout)
-
-                            writer = SummaryWriter(log_dir = "runs/" + "architecture=" + architecture + "_batch=" + str(batch_size) + "_train=" + str(train_split) + 
-                            "_test=" + str(test_split) + "_opt=" + opt + "_lr=" + str(lr) + "_dropout=" + str(dropout))
-
-                            loss = smp.utils.losses.DiceLoss()
-
-                            metrics = [
-                            smp.utils.metrics.IoU(threshold = 0.5)
-                            ]
-
-                            if opt == "adam":
-                                optimizer = torch.optim.Adam([
-                                    dict(params = model.parameters(), lr = lr)])
-
-                            elif opt == "SGD":
-                                optimizer = torch.optim.SGD([
-                                    dict(params = model.parameters(), lr = lr, momentum = 0.9)])
-                            
-                            train_epoch = smp.utils.train.TrainEpoch(
-                                model,
-                                loss = loss,
-                                metrics = metrics,
-                                optimizer = optimizer,
-                                device = "cuda",
-                                verbose = True)
-
-                            valid_epoch = smp.utils.train.ValidEpoch(
-                                model,
-                                loss = loss,
-                                metrics = metrics,
-                                device = "cuda",
-                                verbose = True)
-
-                            # train model for 40 epochs
-                            max_score = 0
-                            
-                            for i in range(0, 20):
-
-                                print("\nEpoch: {}".format(i))
-                                train_logs = train_epoch.run(train_loader)
-                                valid_logs = valid_epoch.run(val_loader)
-
-                                writer.add_scalar("Loss/Train", train_logs["dice_loss"], i)
-                                writer.add_scalar("Loss/Valid", valid_logs["dice_loss"], i)
-
-                                writer.add_scalar("Score/Train", train_logs["iou_score"], i)
-                                writer.add_scalar("Score/Valid", valid_logs["iou_score"], i)
-                                
-                                # do something (save model, change lr, etc.)
-                                if max_score < valid_logs["iou_score"]:
-                                    max_score = valid_logs["iou_score"]
-                                    torch.save(model, "models/" + "architecture=" + architecture + "_batch=" + str(batch_size) + "_train=" + str(train_split) + 
-                            "_test=" + str(test_split) + "_opt=" + opt + "_lr=" + str(lr) + "_dropout=" + str(dropout) + ".pth")
-
-                                if max_score > best_score:
-                                    torch.save(model, "models/best_model.pth")
-                                    torch.save(test_loader, "test_loader_for_inference.pth")
-
-                            writer.flush()                          
+    # train base model
+    train_model(train_loader = train_loader, valid_loader = val_loader, test_loader = test_loader)        
